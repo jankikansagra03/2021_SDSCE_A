6ffdfccbb0240feaade2bf218d1d661c06f1d310
jankikansagra03 <janki.kansagra12@gmail.com>
2023-07-04 14:35:45 +0530

    database and validation

--- app/Http/Controllers/SampleController.php
+++ app/Http/Controllers/SampleController.php
@@ -9,24 +9,42 @@ class SampleController extends Controller
     public function fetch_data_register(Request $req)
     {
         $req->validate([
-            'fn' => 'required|min:3|max:30',
-            'em' => 'required',
-            'pwd' => 'required|min:10|max:16|confirmed',
+            'fn' => 'required|min:3|max:20',
+            'em' => 'required|email',
+            'pwd' => 'required|confirmed|regex:/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,20}$/',
             'pwd_confirmation' => 'required',
             'age' => 'required',
+            'mobile' => 'required|digits:10',
             'gender' => 'required',
+            'hobby' => 'required',
             'qualification' => 'required',
             'address' => 'required',
-            'hobby' => 'required',
-            'pic' => 'required|size:36000|mimes:jpg,png,gif,bmp',
-            'mobile' => 'required|numeric|length:10'
+            'pic' => 'required|max:100|mimes:jpg,png,gif,bmp'
 
         ], [
-            'fn.required' => 'Firstname cannot be empty',
-            'fn.min' => 'Firstname must have more than 3 characters',
-            'fn.max' => 'Firstname cannot have morethan 30 characetsr'
+            'fn.required' => 'Full name cannot be empty',
+            'fn.min' => 'Full name must contain minimum 3 characters',
+            'fn.max' => 'Full name must contain maximum of 30 characters',
+            'em.required' => 'Email address canniot be empty',
+            'em.email' => 'invalid email address',
+            'pwd.required' => 'Password field cannot be empty',
+            'pwd.regex' => 'Password must contain one small letter one capital letter, one number and one special symbol',
+            'pwd.confirmed' => 'Password and Confirm Password must match',
+            'pwd_confirmation.required' => 'Confirm Password must not be empty',
+            'mobile.required' => 'Mobile number cannot be empty',
+            'mobile.digits' => 'Mobile number must conatin only 10 digits',
+            'pic.required' => 'Please select a file to uplaod',
+            'pic.max' => 'Select a file of max 25 KB',
+            'pic.mimes' => 'Select jpg or png or bmp file to uplaod',
+            'hobby.required' => 'Please select a hobby',
+            'qualification.required' => 'Please select a qualification',
+            'address.required' => 'Address field cannot be empty',
+            'gender.required' => 'Please select your Gender',
+            'age.required' => 'Please enter your age'
         ]);
         $hobbies = $req->input('hobby');
+        $pic_name = $req->file('pic')->getClientOriginalName();
+        $req->pic->move('Images/profile_pictures', $pic_name);
         return view('display_registeer', compact('req', 'hobbies'));
     }
 }
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -11,7 +11,7 @@
 <body>
     <form action="FetchRegister" method="post" enctype="multipart/form-data">
         @csrf
-        Enter Name: <input type="text" name="fn" id="fn1">
+        Enter Name: <input type="text" name="fn" id="fn1" value="{{ old('fn') }}">
         <br>
         <span style="color:red">
             @error('fn')
@@ -19,62 +19,109 @@
             @enderror
         </span>
         <br>
-        Enter Email: <input type="email" name="em" id="em1">
+        Enter Email: <input type="email" name="em" id="em1"value="{{ old('em') }}">
         <br>
-
+        <span style="color:red">
+            @error('em')
+                {{ $message }}
+            @enderror
+        </span>
         <br>
-        Enter Password: <input type="password" name="pwd" id="pwd1">
+        Enter Password: <input type="password" name="pwd" id="pwd1" value="{{ old('pwd') }}">
         <br>
-
+        <span style="color:red">
+            @error('pwd')
+                {{ $message }}
+            @enderror
+        </span>
         <br>
-        Enter Confirm Password: <input type="password" name="pwd_confirmation" id="cpwd1">
+        Enter Confirm Password: <input type="password" name="pwd_confirmation" id="cpwd1"
+            value="{{ old('pwd_confirmation') }}">
         <br>
-
+        <span style="color:red">
+            @error('pwd_confirmation')
+                {{ $message }}
+            @enderror
+        </span>
         <br>
-        Enter Age: <input type="number" name="age" id="age1">
+        Enter Age: <input type="number" name="age" id="age1" value="{{ old('age') }}">
         <br>
-
+        <span style="color:red">
+            @error('age')
+                {{ $message }}
+            @enderror
+        </span>
         <br>
-        Enter Mobile Number: <input type="tel" name="mobile" id="mobile">
+        Enter Mobile Number: <input type="tel" name="mobile" id="mobile" value="{{ old('mobile') }}">
         <br>
-
+        <span style="color:red">
+            @error('mobile')
+                {{ $message }}
+            @enderror
+        </span>
         <br>
-        Select Gender: <input type="radio" name="gender" id="gender1" value="Male" /> Male
-        <input type="radio" name="gender" id="gender2" value="Female" /> Female
+        Select Gender: <input type="radio" name="gender" id="gender1" value="Male"
+            @if (old('gender') == 'Male') checked @endif /> Male
+        <input type="radio" name="gender" id="gender2" value="Female"
+            @if (old('gender') == 'Female') checked @endif /> Female
         <br>
-
+        <span style="color:#ff0000">
+            @error('gender')
+                {{ $message }}
+            @enderror
+        </span>
         <br>
         Select your Hobby:
         <br>
-        <input type="checkbox" name="hobby[]" id="hobby1" value="Playing Sports" />Playing Sports
+        <input type="checkbox" name="hobby[]" id="hobby1" value="Playing Sports"
+            @if (is_array(old('hobby')) && in_array('Playing Sports', old('hobby'))) chec0ked @endif />Playing Sports
         <br>
-        <input type="checkbox" name="hobby[]" id="hobby1" value="Reading" />Reading
+        <input type="checkbox" name="hobby[]" id="hobby1" value="Reading"
+            @if (is_array(old('hobby')) && in_array('Reading', old('hobby'))) checked @endif />Reading
         <br>
-        <input type="checkbox" name="hobby[]" id="hobby1" value="Cooking" />Cooking
+        <input type="checkbox" name="hobby[]" id="hobby1" value="Cooking"
+            @if (is_array(old('hobby')) && in_array('Cooking', old('hobby'))) checked @endif />Cooking
         <br>
-
+        <span style="color:red">
+            @error('hobby')
+                {{ $message }}
+            @enderror
+        </span>
         <br>
         Select your Qualification:
         <select name="qualification" id="qualification1">
-            <option value="Under Graduate">Under Graduate</option>
-            <option value="Graduate">Graduate</option>
-            <option value="Post Graduate">Post Graduate</option>
-            <option value="Doctorate">Doctorate</option>
-            <option value="Post Doctorate">Post Doctorate</option>
+            <option value="Under Graduate" @if (old('qualification') == 'Under Graduate') selected @endif>Under Graduate</option>
+            <option value="Graduate" @if (old('qualification') == 'Graduate') selected @endif>Graduate</option>
+            <option value="Post Graduate" @
+            if (old('qualification') == 'Post Graduate') selected @endif>Post Graduate</option>
+            <option value="Doctorate" @if (old('qualification') == 'Doctorate') selected @endif>Doctorate</option>
+            <option value="Post Doctorate" @if (old('qualification') == 'Post Doctorate') selected @endif>Post Doctorate</option>
         </select>
         <br>
-
+        <span style="color:red">
+            @error('qualification')
+                {{ $message }}
+            @enderror
+        </span>
         <br>
         Enter Your address
         <textarea name="address" id="address1">
-
+        {{ old('address') }}
         </textarea>
+        <br>...
+        <span style="color:red">
+            @error('address')
+                {{ $message }}
+            @enderror
+        </span>
         <br>
-
+        Select Profile Picture: <input type="file" name="pic" id="pic1" value="{{ old('pic') }}">
         <br>
-        Select Profile Picture: <input type="file" name="pic" id="pic1">
-        <br>
-
+        <span style="color:red">
+            @error('pic')
+                {{ $message }}
+            @enderror
+        </span>
         <br>
         <input type="submit" value="Register" name="reg" id="reg1">
     </form>
